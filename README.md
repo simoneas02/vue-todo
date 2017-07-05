42c34b56b03b732acc16b7f0df62e92ab58eadb1
simoneas02 <simoneas02@hotmail.com>
2017-07-05 20:28:36 -0300

    starting a simple project Vue TODO

---
+++ README.md
@@ -0,0 +1,37 @@
+# Vue TODO
+
+> A simple TODO List
+
+## Run the project local
+
+**0 -** install the basic dependencies
+
+- [NodeJS](https://nodejs.org/en/)
+
+**1 -** Clone the project and install the dependencies:
+
+```sh
+$ git clone https://github.com/simoneas02/vue-todo
+$ cd vue-todo/
+$ npm install
+```
+
+**2 -** Start development mode:
+
+```sh
+$ npm run dev
+$ npm run build
+```
+
+## Contributing
+
+Find on our [issues](https://github.com/simoneas02/vue-todo/issues/) the next steps of the project ;) 
+Want to contribute? [Follow these recommendations](https://https://github.com/simoneas02/vue-todo/issues/blob/master/CONTRIBUTING.md).
+
+## History
+
+See [Releases](https://github.com/simoneas02/vue-todo/releases) for detailed changelog.
+
+## License
+
+[MIT License](https://github.com/simoneas02/vue-todo/blob/master/license.md) © [Simone Amorim](https://simoneas02.github.io)
---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="container" role="main" id="app">
+        <div class="content">
+            <img class="content__logo" src="./assets/logo.png">
+            <h1 class="content__title" role="heading">TODO</h1>
+        </div>
+        
+        <div class="task">
+            <input class="task__input" role="textbox" v-model="newItem" type="text">
+            <button class="task__button" role="button" v-on:click="addItem(newItem)">+</button>
+            
+            <ul class="task__list" role="list">
+                <li class="task__list__item" role="listitem"
+                    v-for="(item, index) in list" :key="index">
+                    <button class="task__list__button" v-on:click="removeItem(index)">-</button>
+                    {{ item.text }}
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data () {
+      return {
+        list: [ ],
+        newItem: ""
+      }
+    },
+    methods: {
+        addItem (newItem) {
+            if(newItem){
+                this.list.push({
+                text: newItem
+                })
+            }
+            this.newItem= "";
+        },
+
+        removeItem (index) {
+            const newList = this.list.filter(function(item, i) {
+                return index !== i;
+            })
+            this.list = newList;
+        }
+    }
+}
+</script>
+
+<style>
+    .container {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
+    .content {
+        width: 50%;
+        display: flex;
+        margin: 0 auto;
+        justify-content: center;
+    }
+
+    .content__logo {
+        width: 100px;
+        height: 100px;
+        transform: scale(.7);
+    }
+
+    .content__title {
+        text-align: start;
+        margin-top: 40px;
+        font-weight: normal;
+    }
+
+    .task {
+        width: 50%;
+        margin: 0 auto;
+    }
+
+    .task__list {
+        list-style-type: none;
+        padding: 0;
+        width: 400px;
+        margin: 10px auto;
+    }
+
+    .task__list__item {
+      width: 50%;
+      margin: 0 auto;
+      text-align: left;
+    }
+
+</style>
